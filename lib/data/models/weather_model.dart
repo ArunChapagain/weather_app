bf6ceb6ca144c1c366059762d91cd13834f38af8
Arun Chapagain <chaparun2074@gmail.com>
2023-12-22 22:12:17 +0545

    test : written test for fromJson methed

--- lib/data/models/weather_model.dart
+++ lib/data/models/weather_model.dart
@@ -1,9 +1,7 @@
-
 import 'package:weather_app/domain/entities/weather.dart';
 
-class WeatherModel extends WeatherEntity{
-
- const  WeatherModel({
+class WeatherModel extends WeatherEntity {
+  const WeatherModel({
     required String cityName,
     required String main,
     required String description,
@@ -12,14 +10,24 @@ class WeatherModel extends WeatherEntity{
     required int pressure,
     required int humidity,
   }) : super(
-    cityName: cityName,
-    main: main,
-    description: description,
-    iconCode: iconCode,
-    temperature: temperature,
-    pressure: pressure,
-    humidity: humidity,
-  );
+          cityName: cityName,
+          main: main,
+          description: description,
+          iconCode: iconCode,
+          temperature: temperature,
+          pressure: pressure,
+          humidity: humidity,
+        );
 
-  
+  factory WeatherModel.fromJson(Map<String, dynamic> jsonMap) {
+    return WeatherModel(
+      cityName: jsonMap['name'],
+      main: jsonMap['weather'][0]['main'],
+      description: jsonMap['weather'][0]['description'],
+      iconCode: jsonMap['weather'][0]['icon'],
+      temperature: jsonMap['main']['temp'].toDouble(),
+      pressure: jsonMap['main']['pressure'],
+      humidity: jsonMap['main']['humidity'],
+    );
+  }
 }
--- test/data/models/weather_model_test.dart
+++ test/data/models/weather_model_test.dart
@@ -1,31 +1,45 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:weather_app/data/models/weather_model.dart';
 import 'package:weather_app/domain/entities/weather.dart';
 
+import '../../helper/json_reader.dart';
+
 void main() {
+  setUp(() {});
 
-  setUp(() {
-
-
-  });
 
   const tWeatherModel = WeatherModel(
-    cityName: 'London',
-    main: 'Clouds',
-    description: 'scattered clouds',
-    iconCode: '03d',
-    temperature: 280.32,
-    pressure: 1012,
-    humidity: 81,
+    cityName: 'New York',
+    main: 'Clear',
+    description: 'clear sky',
+    iconCode: '01d',
+    temperature: 271.71,
+    pressure: 1033,
+    humidity: 56,
   );
 
-test('should be a subclass of WeatherEntity entity',
-() async {
+  test('should be a subclass of WeatherEntity entity', () async {
 // arrange
 // act
 // assert
-expect(tWeatherModel, isA<WeatherEntity>());
+    expect(tWeatherModel, isA<WeatherEntity>());
   });
+
+  test('should return valid model from json', () async {
+// arrange
+    final Map<String, dynamic> jsonMap = json.decode(
+      readJson('helper\\dummy_data\\dummy_weather_response.json'),
+    );
+    print(jsonMap); // to see the jsonMap
+// act
+    final result = WeatherModel.fromJson(jsonMap);
+// assert
+    expect(result, tWeatherModel);
+  });
+
 
 
 }
