a0ffd405f9ceba5ca5f1e6eeb6ca03adf03b884e
Arun Chapagain <chaparun2074@gmail.com>
2023-12-24 16:13:25 +0545

    :test : weather_repository_impl test failed

--- test/helper/test_helper.dart
+++ test/helper/test_helper.dart
@@ -1,9 +1,13 @@
 import 'package:mockito/annotations.dart';
+import 'package:weather_app/data/data_source/remote_data_source.dart';
 import 'package:weather_app/domain/repositary/weather_repository.dart';
 import 'package:http/http.dart' as http;
 
 @GenerateMocks(
-  [WeatherRepository],
+  [
+    WeatherRepository,
+    WeatherRemoteDataSource,
+  ],
   customMocks: [
     MockSpec<http.Client>(as: #MockHttpClient),
   ],
