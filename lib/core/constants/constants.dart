a6e36e30ad7a76b2c0de072b42711917d9bd1990
Arun Chapagain <99020948+ArunChapagain@users.noreply.github.com>
2024-02-26 19:22:55 +0545

    minor changes

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,7 +1,7 @@
 class Url {
   static const String baseUrl =
       'https://api.openweathermap.org/data/2.5/weather';
-  static const String apikey = 'cc95d932d5a45d33a9527d5019475f2c';
+  static const String apikey =""//your api key
   static String currentWeatherByCityName(String cityName) =>
       '$baseUrl?q=$cityName&appid=$apikey';
   static String weatherIcon(String iconCode) =>
